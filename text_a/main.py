41a05d876c78c6fdd9df2697ee37ee89f64a638d
DEB <112969476+Mot932@users.noreply.github.com>
2023-06-11 18:38:51 +0300

    Update main.py

--- text_a/main.py
+++ text_a/main.py
@@ -1,26 +1,30 @@
 class TextAnalyser:
-    def __init__(self, file_name=None):
-        """ вызывает цепочку методов """
+    def _init_(self, file_name=None):
+        """вызывает цепочку методов"""
         if file_name is None:
             raise Exception("Не указан файл для анализа!")
         self.read_file(file_name)
-        # TODO: Здесь нужен метод подготовки текста: вся строка текста должна стать строчными буквами
+        self.prepare_text()
         self.print_text()
 
     def read_file(self, file_name):
-        """ пытается открыть файл и считать его в строку """
+        """пытается открыть файл и считать его в строку"""
         try:
             with open(file_name, "r", encoding="UTF-8") as file:
-                self.file = file  # здесь получается файловый объект
-                self.text = self.file.read()  # здесь получается строка текста
+                self.file = file
+                self.text = self.file.read()
         except FileNotFoundError:
             raise Exception(f"Файл {file_name} не найден!")
-        
-        # TODO: бросить исключение, если прочитанный файл оказался пуст
+
+        if not self.text.strip():
+            raise Exception("Прочитанный файл пуст!")
+
+    def prepare_text(self):
+        """приводит текст к нижнему регистру"""
+        self.text = self.text.lower()
 
     def print_text(self):
-        """ выводит строку текста на экран """
+        """выводит строку текста на экран"""
         print(self.text)
-
 
 TextAnalyser(file_name="text.txt")
