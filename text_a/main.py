587b0f2b64d0afa12f80cc9c6fe640e8bdb502e8
DEB <112969476+Mot932@users.noreply.github.com>
2023-06-12 13:07:09 +0300

    Add files via upload

--- text_a/main.py
+++ text_a/main.py
@@ -1,6 +1,6 @@
 class TextAnalyser:
-    def _init_(self, file_name=None):
-        """вызывает цепочку методов"""
+    def __init__(self, file_name=None):
+        """ вызывает цепочку методов """
         if file_name is None:
             raise Exception("Не указан файл для анализа!")
         self.read_file(file_name)
@@ -8,11 +8,11 @@ class TextAnalyser:
         self.print_text()
 
     def read_file(self, file_name):
-        """пытается открыть файл и считать его в строку"""
+        """ пытается открыть файл и считать его в строку """
         try:
             with open(file_name, "r", encoding="UTF-8") as file:
-                self.file = file
-                self.text = self.file.read()
+                self.file = file  # здесь получается файловый объект
+                self.text = self.file.read()  # здесь получается строка текста
         except FileNotFoundError:
             raise Exception(f"Файл {file_name} не найден!")
 
@@ -21,10 +21,11 @@ class TextAnalyser:
 
     def prepare_text(self):
         """приводит текст к нижнему регистру"""
-        self.text = self.text.lower()
+        self.text = self.text.lower()        
 
     def print_text(self):
-        """выводит строку текста на экран"""
+        """ выводит строку текста на экран """
         print(self.text)
+
 
 TextAnalyser(file_name="text.txt")
